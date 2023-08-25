1d3e55ea41e7b7ac7a546a58b2ebacc834132c1c
ashesash <aishwarya.s@live.com>
2023-08-25 22:35:44 +1000

    spotify extraction from playlist functions added

---
+++ spotifyToYoutube.py
@@ -0,0 +1,11 @@
+#coding: utf-8
+import json
+# Spotify library
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+# URL conversions
+import urllib.request
+import bs4
+# Youtube
+import youtube
+
