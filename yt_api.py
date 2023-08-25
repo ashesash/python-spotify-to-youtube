964f0b9636ba0725125f156f557f7b0b05774630
ashesash <aishwarya.s@live.com>
2023-08-25 22:55:44 +1000

    youtube api connected to script

---
+++ yt_api.py
@@ -0,0 +1,27 @@
+from googleapiclient.discovery import build
+from credentials import yt_api
+from spotify_extract import query_builder
+import time
+
+def makePublicService(api_key):
+    service = build(serviceName='youtube',version='v3',developerKey=api_key)
+    return service
+
+
+def getVideoIds(queris,api):
+    service = makePublicService(api)
+    ids = []
+    for i in range(len(queris)):
+        request = service.search().list(part="snippet",
+                maxResults=2,
+                q = queris[i]
+            )
+        response = request.execute()
+        videoid = response['items'][0]['id']['videoId']
+        print(videoid)
+
+        ids.append(videoid)
+        time.sleep(3)
+    return ids
+
+# print(getVideoIds(queries))
