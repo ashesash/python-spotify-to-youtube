88b2aa22c6551f3a36b96ce7510b863f0a818e78
ashesash <aishwarya.s@live.com>
2023-11-03 11:08:17 +1100

    initial setup

--- spotify_extract.py
+++ spotify_extract.py
@@ -11,7 +11,7 @@ spotify_api = spotipy.Spotify(
 )
 
 # fetch the playlist
-def fetch_playlist_by_id(api, id):
+def fetch_playlist(api, id):
     playlist_response = api.playlist(playlist_id=id)
     name = playlist_response['name']
     playlist_items = playlist_response['tracks']['items']
@@ -20,11 +20,11 @@ def fetch_playlist_by_id(api, id):
 # extract song data from playlist
 def extract_data(api, items):
     data = []
-    for i in items:
+    for track in items:
         item = dict.fromkeys(['track_name', 'artist_name', 'album_name'])
-        track_name = i['track']['name']
-        artist_name = i['track']['artists'][0]['name']
-        album_name = i['track']['album']['name']
+        track_name = track['track']['name']
+        artist_name = track['track']['artists'][0]['name']
+        album_name = track['track']['album']['name']
 
         item['track_name'] = track_name
         item['artist_name'] = artist_name
